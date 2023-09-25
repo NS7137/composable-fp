79e650bd0ee6b684f45b98c9db7ebb30a45a8350
N∩S <sunqiwei@gmail.com>
2023-09-25 16:12:41 +0800

    Update index.js

--- weather/index.js
+++ weather/index.js
@@ -1,30 +1,27 @@
 import apiKey from './apiKey.js'
-import { getWeatherItems } from './model.js'
+import { getWeatherItems } from './weather_model.js'
 
-const populateUI = (zip) =>
+const populateUI = zip =>
   getWeatherItems({ zip, apiKey })
-    .map(weathers => weathers.map(toLi))
+    .map(weathers => ({ name: weathers.city.name, items: weathers.items.map(toLi) }))
 
 const toLi = weather =>
-  `<li>${weather.dt} ${weather.temp}</li>`
+  `<li>${weather.dt} -> ${weather.temp}</li>`
 
-// =============================================================
-
+// ==================================================
 const app = () => {
-  const goButton = document.getElementById('go')
   const input = document.getElementById('zip')
+  const goButton = document.getElementById('go')
+  const city = document.getElementById('city')
   const results = document.getElementById('results')
 
   goButton.addEventListener('click', () => {
     const zip = input.value.trim()
     populateUI(zip).fork(console.error, html => {
-      results.innerHTML = html
+      city.innerHTML = html.name
+      results.innerHTML = html.items
     })
   })
 }
 
 app()
-
-// =============================================================
-
-
